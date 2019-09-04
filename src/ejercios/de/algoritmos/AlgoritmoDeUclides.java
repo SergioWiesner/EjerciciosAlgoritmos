ec4bd8654c6fd7a8ea1a70b8b9aac54302661e8e
Sergio Wiesner <webdev@codwelt.com>
2019-09-03 19:46:43 -0500

    nueva transformación
    nueva transformación

--- src/ejercios/de/algoritmos/AlgoritmoDeUclides.java
+++ src/ejercios/de/algoritmos/AlgoritmoDeUclides.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class AlgoritmoDeUclides {   
 
     public static int euclides(){
-        int t, a, b;
+        int t, a, b; // 3
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingresa la base a potenciar");
-        a = scan.nextInt();
+        a = scan.nextInt(); // 1
         System.out.println("Ingresa el exponente");
-        b = scan.nextInt();
+        b = scan.nextInt(); // 1
         
-        while(a > 0){
-            t = a;
-            a = b % a;
-            b = t;
-        }
+        while(a > 0){ // 1 
+            t = a; // 1
+            a = b % a; // 2
+            b = t; // 1
+        } // 1
         System.out.println(b);
-        return b;
+        return b; // 1
         
-        // 5+6a%b
+        // 6 + 5a + 1
     } 
 }
