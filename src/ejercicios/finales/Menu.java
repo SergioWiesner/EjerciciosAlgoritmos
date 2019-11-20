abea430119a6702e2e97efef165ccd394a5b681a
Sergio Wiesner <sergio.wiesner@cirrus.net.co>
2019-11-20 17:31:33 -0500

    actualizacion del ejercicio de poda

--- src/ejercicios/finales/Menu.java
+++ src/ejercicios/finales/Menu.java
@@ -1,15 +1,17 @@
 package ejercicios.finales;
+
 import java.util.Scanner;
 
 public class Menu {
-            public void menu(){
+
+    public void menu() {
         int opc = 0;
         System.out.println("\t Ejercicios final \t \n\n\n");
         System.out.println("2. Ejercicio de varillas.");
         System.out.println("3. Ejercicio de ramificacion y poda.");
         Scanner scan = new Scanner(System.in);
         opc = scan.nextInt();
-        switch(opc){
+        switch (opc) {
             case 1:
 
                 break;
@@ -17,7 +19,7 @@ public class Menu {
                 Varilla Varilla = new Varilla();
                 Varilla.init();
                 break;
-             case 3:
+            case 3:
                 RamificacionPoda RP = new RamificacionPoda();
                 RP.init();
                 break;
--- src/ejercicios/finales/RamificacionPoda.java
+++ src/ejercicios/finales/RamificacionPoda.java
@@ -1,4 +1,4 @@
-package ejercicios.finales;
+package parcial;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -22,6 +22,9 @@ public class RamificacionPoda {
         this.tabla = new int[personas][tareas];
         tablero(personas, tareas);
         mostrarTablas(this.tabla);
+        int resultado = conviancionOptima(this.tabla, 0, 0, 0);
+        
+        System.out.println("Este es el resultado -> "+resultado);
     }
 
     public void tablero(int personas, int tareas) {
@@ -32,12 +35,11 @@ public class RamificacionPoda {
             }
         }
     }
-    
-    
-     public void mostrarTablas(int[][] tabla) {
+
+    public void mostrarTablas(int[][] tabla) {
         String muestra = "\n\n";
         for (int a = 0; a < this.tabla.length; a++) {
-            muestra += "[Persona "+a+"]\t";
+            muestra += "[Persona " + (a + 1) + "]\t";
             for (int b = 0; b < this.tabla[a].length; b++) {
                 muestra += "[" + this.tabla[a][b] + "]\t";
             }
@@ -45,9 +47,36 @@ public class RamificacionPoda {
         }
         System.out.println(muestra + "\n");
     }
-     
-     
-    public void conviancionOptima(){
-    
+
+    public int conviancionOptima(int[][] tableroactual, int persona, int tarea, int totalsumatoria) {
+
+        if (persona >= tableroactual.length || tarea >= tableroactual[0].length) {
+            return 0;
+        }
+        
+        int actual = tableroactual[persona][tarea];
+        int siguientepersona = 0;
+        int siguientetarea = 0;
+
+        if (persona < tableroactual.length) {
+            System.out.println("Entra en persona " + (persona + 1) + " En  tarea -> " + tarea);
+            siguientepersona = conviancionOptima(tableroactual, (persona + 1), tarea, totalsumatoria);
+            if(siguientepersona != 0){
+            if ((totalsumatoria + siguientepersona) < (totalsumatoria + actual)) {
+                System.out.println("Persona -> " + (persona + 1) + " En la tarea -> " + tarea);
+                totalsumatoria = totalsumatoria + siguientepersona;
+            }
+            }
+        }
+
+        if (tarea < tableroactual[persona].length) {
+            System.out.println("Entra en tarea ------------> " + tarea);
+            siguientetarea = conviancionOptima(tableroactual, persona, (tarea + 1), totalsumatoria);
+            if ((totalsumatoria + siguientetarea) < (totalsumatoria + actual)) {
+                totalsumatoria = totalsumatoria + siguientetarea;
+            }
+        }
+
+        return totalsumatoria;
     }
 }
