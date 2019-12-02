686d1e4688ddc7ec611b445289150fa8f3f7eeca
Sergio Wiesner <sergio.wiesner@cirrus.net.co>
2019-12-02 17:13:49 -0500

    no alcance a avanzar de a mucho en ramificacion y poda

--- src/ejercicios/finales/RamificacionPoda.java
+++ src/ejercicios/finales/RamificacionPoda.java
@@ -19,16 +19,12 @@ public class RamificacionPoda {
 
         System.out.println("ingrese la cantidad de personas");
         personas = in.nextInt();
-        System.out.println("ingrese la cantidad de tareas");
-        tareas = in.nextInt();
-
-        this.tabla = new int[personas][tareas];
-        this.tablabanneos = new int[personas][tareas];
-        tablero(personas, tareas);
-
+        this.tabla = new int[personas][personas];
+        this.tablabanneos = new int[personas][personas];
+        tablero(personas, personas);
         mostrarTablas(this.tabla);
         int[] resultado = new int[3];
-        resultado = conviancionOptima(this.tabla, 0, 0, 0);
+        resultado = conviancionOptima(0, 0, 0);
 
         System.out.println("\n\n Este es el resultado -> " + resultado[0]);
         init();
@@ -59,9 +55,11 @@ public class RamificacionPoda {
         System.out.println(muestra + "\n");
     }
 
-    public int[] conviancionOptima(int[][] tableroactual, int persona, int tarea, int totalsumatoria) {
-        int[] manejador = new int[3];
-        manejador[0] = totalsumatoria + tableroactual[persona][tarea];// acumulativo actual
+    
+    public int[] convinacionOptima(int persona, int tarea, int totalsumatoria){
+    
+    int[] manejador = new int[3];
+        manejador[0] = totalsumatoria + this.tabla[persona][tarea];// acumulativo actual
         manejador[1] = persona; //personas
         manejador[2] = tarea; //tarea
         //System.out.println("VALOR CON QUE ENTRA "+manejador[0]+" EN PERONSA  "+manejador[1]+" Y TAREA "+manejador[2]);
@@ -69,8 +67,8 @@ public class RamificacionPoda {
         int[] siguientepersona = new int[3];
         int[] siguientetarea = new int[3];
 
-        if ((persona + 1) < tableroactual.length) {
-            siguientepersona = conviancionOptima(tableroactual, (persona + 1), tarea, totalsumatoria);
+        if ((persona + 1) < this.tabla.length) {
+            siguientepersona = convinacionOptima((persona + 1), tarea, totalsumatoria);
             if (siguientepersona[0] < manejador[0]) {
               //  System.out.println("La siguiente persona ->" + (siguientepersona[1] + 1) + " es menos que la persona actual -> " + (manejador[1] + 1) + " con valor de siguiente persona -> " + siguientepersona[0] + " y persona actual ->" + manejador[0]);
                 manejador[0] = siguientepersona[0];
@@ -78,29 +76,55 @@ public class RamificacionPoda {
             }
         }
 
-        if ((tarea + 1) < tableroactual[0].length) {
-            siguientetarea = conviancionOptima(tableroactual, 0, (manejador[2] + 1), manejador[0]);
-            System.out.println("Entra con "+manejador[0]+" y sale con "+siguientetarea[0]);
+        if ((tarea + 1) < this.tabla.length) {
+            siguientetarea = convinacionOptima(0, (manejador[2] + 1), manejador[0]);
+           // System.out.println("Entra con "+manejador[0]+" y sale con "+siguientetarea[0]);
             if (siguientetarea[2] == manejador[2] && siguientetarea[0] < manejador[0]) {
-                System.out.println("En la tarea ->" + siguientetarea[2] + " la persona -> " + siguientetarea[1] + " tuvo menor valor -> " + siguientetarea[0]);
+             //   System.out.println("En la tarea ->" + siguientetarea[2] + " la persona -> " + siguientetarea[1] + " tuvo menor valor -> " + siguientetarea[0]);
                 manejador[0] = siguientetarea[0];
                 manejador[1] = siguientetarea[1];
             } 
         }
         
-        System.out.println("RETORNO -> " + manejador[0]);
+     //   System.out.println("RETORNO -> " + manejador[0]);
         return manejador;
+    
     }
-
-    public boolean averiguarBaneo(int[][] tablaban) {
-        for (int a = 0; a < tablaban.length; a++) {
-            for (int b = 0; b < tablaban[a].length; b++) {
-                if (tablaban[a][1] == 1) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
+    
+    
+    
+//    public int conviancionOptima(int persona, int tarea, int totalsumatoria) {
+//        int sumatoriaactual = totalsumatoria + this.tabla[persona][tarea];
+//        int ejepersonas1 = 0;
+//        int ejepersonas2 = 0;
+//        int ejetareas = 0;
+//        
+//        if(persona == this.tabla.length){
+//            return sumatoriaactual;
+//        }
+//        
+//                
+//        if ((persona + 1) <= this.tabla.length) {
+//            ejepersonas1 = conviancionOptima((persona + 1), tarea, totalsumatoria);
+//        }
+//        
+//        if ((persona + 2) <= this.tabla.length) {
+//            ejepersonas2 = conviancionOptima((persona + 2), tarea, totalsumatoria);
+//        }
+//        
+//        
+//        if(ejepersonas1 < ejepersonas2){
+//            return ejepersonas1;
+//        }else{
+//            return ejepersonas2;
+//        }
+//        
+//        if ((tarea + 1) < this.tabla.length) {
+//            ejetareas = conviancionOptima(0, (tarea + 1), sumatoriaactual);
+//        }
+//        
+//     
+//        return ejepersonas+ejetareas;
+//        
+//    }
 }
